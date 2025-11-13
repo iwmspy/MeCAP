cdc97cfffd1abd38b15dbcdd921f1924b46b0a79
iwmspy <iwasaki.yuto.jz4@ms.naist.jp>
2025-11-13 04:37:46 +0000

    Refactor: Update license information across multiple files

--- src/core_modules/evaluate.py
+++ src/core_modules/evaluate.py
@@ -1,4 +1,25 @@
-# -*- coding: utf-8 -*-
+# MIT License
+#
+# Copyright (c) 2025 Yuto Iwasaki
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # End-to-end training script for single-atom regression on Uni-Mol with pre-optimized SDF structures.
 # - Reads SDF coordinates directly (no confgen) with filename-only cells in CSV and --sdf_dir
 # - Supports per-row SDF filenames or a single SDF with multiple molecules
